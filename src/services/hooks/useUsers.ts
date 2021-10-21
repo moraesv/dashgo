8fe18e8b838c7b3f9fe2560e9b58d71714eda434
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-10-21 18:30:33 -0300

    add useMutation

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryOptions } from 'react-query';
 import { api } from '../api';
 
 type User = {
@@ -38,8 +38,14 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   return { users, totalCount };
 }
 
-export function useUsers(page: number) {
+export function useUsers(
+  page: number,
+  options: UseQueryOptions = {} as UseQueryOptions
+) {
+  console.log(options);
+
   return useQuery(['users', page], () => getUsers(page), {
     staleTime: 1000 * 60 * 10,
+    ...options,
   });
 }
